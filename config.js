ab1f88a07c8f1613c197dfc5d99d05e557993cee
Jozef Dransfield <Jozef.Dransfield@me.com>
2013-04-25 15:24:47 +0100

    First Commit

---
+++ config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var fs = require("fs");
+var _ = require("underscore")._;
+
+module.exports.Config = function (locations) {
+    var this_ = this;
+    _(locations).each(function (location) {
+        try {
+            var properties = JSON.parse(fs.readFileSync(location));
+            _(properties).each(function (value, key) {
+                this_[key] = value;
+            });
+        } catch (error) {
+            console.log("Failed to load config at: " + location + " because of: " + error.message);
+        }
+    });
+};
